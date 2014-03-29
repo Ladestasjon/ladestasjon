30f2696d5b070dd2a13a20b4bbf75a5cd8d03cdf
Roar Bjurstrom <roar.bjurstrom@gmail.com>
2014-03-29 10:52:03 +0100

    La til kartet

---
+++ js/main.js
@@ -0,0 +1,27 @@
+var ladestasjon = ladestasjon || {};
+
+
+ladestasjon.main = (function (maps) {
+    var $container,
+        mapCanvas;
+
+    function init() {
+        var mapOptions = {
+            center: new maps.LatLng(44.5403, -78.5463),
+            zoom: 8,
+            mapTypeId: maps.MapTypeId.ROADMAP
+        };
+        var map = new maps.Map(mapCanvas, mapOptions);
+
+    }
+
+    function main(container) {
+        $container  = container;
+        mapCanvas = document.getElementById('map_canvas');
+        init();
+    }
+
+    return {
+        main: main
+    };
+}(google.maps));
